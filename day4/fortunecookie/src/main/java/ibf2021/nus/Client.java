cffeb5703db38195df127ac4749d4ee99e9d8e3d
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-07 11:05:59 +0800

    Added some print prompts for the client.

--- day4/fortunecookie/src/main/java/ibf2021/nus/Client.java
+++ day4/fortunecookie/src/main/java/ibf2021/nus/Client.java
@@ -1,9 +1,11 @@
 package ibf2021.nus;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -13,12 +15,6 @@ public class Client {
     private String serverAddress;
     private Socket socket;
     private String fortuneMessage;
-
-    public static void main(String[] args) throws IOException {
-        Client client = new Client("localhost", 8888);
-        client.sendToServer("get-cookie");
-        client.closeSocket();
-    }
 
     public Client(String serverAddress, int serverPort) {
         this.serverAddress = serverAddress;
@@ -31,6 +27,20 @@ public class Client {
             e.printStackTrace();
         }
 
+    }
+
+    public void start(InputStream inputStream) throws IOException {
+        if (inputStream != System.in) {
+            this.testInput(inputStream);
+        } else {
+            System.err.println("Welcome. Enter 'get-cookie' to get a fortune cookie!");
+            while (true) {
+                String input = this.getInput(inputStream);
+                if (!this.processInput(input.trim())) {
+                    return;
+                }
+            }
+        }
     }
 
     private String receiveFromServer() throws IOException {
@@ -61,4 +71,47 @@ public class Client {
         socket.close();
     }
 
+    private String getInput(InputStream inputStream) {
+        String input = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            input = br.readLine();
+            return input;
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return input;
+    }
+
+    private void testInput(InputStream inputStream) throws IOException {
+        String input = this.getInput(inputStream);
+        this.processInput(input);
+    }
+
+    private boolean processInput(String input) throws IOException {
+        if (input.isBlank()) {
+            System.err.println("Input cannot be blank.");
+            return false;
+        }
+
+        switch (input.toLowerCase()) {
+            case "get-cookie":
+                this.sendToServer(input);
+                System.out.println("Getting your fortune cookie. Enter 'cookie-text' to open the cookie");
+                break;
+            case "cookie-text":
+                this.fortuneMessage = receiveFromServer();
+                System.out.println(fortuneMessage);
+                System.out.println("Enter 'get-cookie' to get another cookie, or enter 'close' to exit.");
+                break;
+            case "close":
+                this.sendToServer(input);
+                this.closeSocket();
+                return false;
+            default:
+                System.out.println("Command not recognized. Please enter only get-cookie or cookie-text.");
+        }
+
+        return true;
+    }
 }
