83189c8db08e141946cf2b776b6f87ba4ed089de
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-07 22:26:49 +0800

    Added some validation for args input.

--- day4/fortunecookie/src/main/java/ibf2021/nus/App.java
+++ day4/fortunecookie/src/main/java/ibf2021/nus/App.java
@@ -2,30 +2,48 @@ package ibf2021.nus;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
-            System.out.println("Error. No arguments inputted");
+            System.out.println("Error. No arguments inputed");
             return;
         } else if (args[0].equalsIgnoreCase("fc.server")) {
+            /**
+             * Creates an instance of the server.
+             * args should be in the format fc.server <port> <path of txt file containing
+             * fortune cookie sayings>
+             */
+            if (args.length != 3) {
+                System.err.println("Invalid argument length");
+                return;
+            }
             int serverPort = Integer.parseInt(args[1]);
-            String cookieJar = "./" + args[2];
-            Path cookieJarFile = Paths.get(cookieJar);
+            String cookieJar = args[2];
+            Path cookieJarFile = Path.of(cookieJar);
             Server server = new Server(serverPort, cookieJarFile);
             server.startServer();
             System.out.println("Server started successfully.");
             server.startConnection();
         } else if (args[0].equalsIgnoreCase("fc.client")) {
+            /**
+             * Creates an instance of the client.
+             * args should be in the format fc.client <host:port>
+             */
+            if (args.length != 2) {
+                System.err.println("Invalid argument length");
+                return;
+            }
             String[] argsParts = args[1].split(":");
-            Client client = new Client(argsParts[0], Integer.parseInt(argsParts[1]));
+            try {
+                Client client = new Client(argsParts[0], Integer.parseInt(argsParts[1]));
+                client.start(System.in);
+            } catch (NumberFormatException e) {
+                System.err.println("Error. Please enter a valid port.");
+                return;
+            }
             System.out.println("Client successfully started.");
-            client.start(System.in);
+
         }
     }
 }
