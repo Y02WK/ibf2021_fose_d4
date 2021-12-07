3e74dcf4b9b914ae6cf5f2208893cff0f67cf4c6
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-07 11:07:41 +0800

    Changed the structure and method of how data is read and written from the socket.

--- day4/fortunecookie/src/main/java/ibf2021/nus/ConnectionHandler.java
+++ day4/fortunecookie/src/main/java/ibf2021/nus/ConnectionHandler.java
@@ -1,16 +1,15 @@
 package ibf2021.nus;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedWriter;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class ConnectionHandler implements Runnable {
-    private final Socket socket;
+    private Socket socket;
     private Cookie cookieJar;
 
     // constructor
@@ -22,28 +21,45 @@ public class ConnectionHandler implements Runnable {
     @Override
     public void run() {
         try {
-            System.out.println(this.receiveFromClient());
+            receiveFromClient();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
     }
 
-    private void sendToClient(String message) throws IOException {
+    private void sendToClient(DataOutputStream dos, String message) throws IOException {
+        // OutputStream os = socket.getOutputStream();
+        // DataOutputStream dos = new DataOutputStream(os);
+        dos.writeUTF(message);
+        dos.flush();
+    }
+
+    private void receiveFromClient() throws IOException {
         OutputStream os = socket.getOutputStream();
-        OutputStreamWriter osw = new OutputStreamWriter(os);
-        BufferedWriter bw = new BufferedWriter(osw);
-        bw.write(message);
-        bw.flush();
-    }
-
-    private String receiveFromClient() throws IOException {
+        DataOutputStream dos = new DataOutputStream(os);
         InputStream is = socket.getInputStream();
         BufferedInputStream bis = new BufferedInputStream(is);
         DataInputStream dis = new DataInputStream(bis);
+        while (is.available() == 0) {
+            String message = dis.readUTF();
+            processRequest(dos, message);
+        }
+    }
 
-        String message = dis.readUTF();
-        return message;
+    private void processRequest(DataOutputStream dos, String request) throws IOException {
+        switch (request) {
+            case "get-cookie":
+                String response = cookieJar.getCookie();
+                sendToClient(dos, response);
+                return;
+            case "close":
+                closeSocket();
+            default:
+                sendToClient(dos, "Server error");
+        }
+    }
+
+    private void closeSocket() throws IOException {
+        socket.close();
     }
 }
