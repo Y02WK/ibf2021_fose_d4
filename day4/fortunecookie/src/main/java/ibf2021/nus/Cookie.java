068c294da919186a09d124e760f71a97b5daf29c
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-13 11:57:03 +0800

    Refactored code.

--- day4/fortunecookie/src/main/java/ibf2021/nus/Cookie.java
+++ day4/fortunecookie/src/main/java/ibf2021/nus/Cookie.java
@@ -5,21 +5,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Cookie {
     private Path cookieJarPath;
-    private ArrayList<String> cookieJar;
+    private List<String> cookieJar;
 
     public Cookie(Path cookieJarPath) {
         this.cookieJarPath = cookieJarPath;
         this.cookieJar = loadCookies();
     }
 
-    protected ArrayList<String> loadCookies() {
+    protected List<String> loadCookies() {
         // Loads all fortunes from the fortunecookie file into an ArrayList
         String item;
-        ArrayList<String> cookieLoader = new ArrayList<String>();
+        List<String> cookieLoader = new ArrayList<String>();
         try {
             BufferedReader reader = Files.newBufferedReader(cookieJarPath);
             while ((item = reader.readLine()) != null) {
